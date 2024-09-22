876e9cde9f0011eabb7a684d8855125266ea772f
Pablo Frank <jgfpablo@gmail.com>
2024-09-22 08:58:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express"); // Importar Express
+const app = express(); // Crear una instancia de Express
+const port = process.env.PORT || 3000; // Asignar el puerto
+
+// Ruta básica que devuelve "hola"
+app.get("/", (req, res) => {
+    res.send("hola");
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
