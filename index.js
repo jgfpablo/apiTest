54306955ffa0a1b0636589b92a231987ecaba3a9
Pablo Frank <jgfpablo@gmail.com>
2024-09-27 11:02:04 -0300

    cosas

--- index.js
+++ index.js
@@ -28,6 +28,61 @@ allProducts = [
         oferta: "no",
         precio: 0,
     },
+    {
+        id: 3,
+        nombre: "charmander",
+        peso: 8.5,
+        tiempo: 55,
+        descripcion:
+            "Un Pokémon de tipo fuego con una llama en la punta de su cola.",
+        colores: ["naranja", "rojo", "amarillo"],
+        oferta: "no",
+        precio: 150,
+    },
+    {
+        id: 4,
+        nombre: "squirtle",
+        peso: 9.0,
+        tiempo: 50,
+        descripcion:
+            "Un Pokémon de tipo agua con un caparazón que lo protege de los ataques.",
+        colores: ["azul", "marrón", "blanco"],
+        oferta: "si",
+        precio: 180,
+    },
+    {
+        id: 5,
+        nombre: "jigglypuff",
+        peso: 5.5,
+        tiempo: 45,
+        descripcion:
+            "Un Pokémon de tipo normal conocido por su habilidad de cantar y poner a dormir a los demás.",
+        colores: ["rosa", "blanco", "negro"],
+        oferta: "si",
+        precio: 130,
+    },
+    {
+        id: 6,
+        nombre: "meowth",
+        peso: 4.2,
+        tiempo: 40,
+        descripcion:
+            "Un Pokémon de tipo normal con habilidades para robar y recolectar monedas.",
+        colores: ["beige", "negro", "marrón"],
+        oferta: "no",
+        precio: 120,
+    },
+    {
+        id: 7,
+        nombre: "psyduck",
+        peso: 19.6,
+        tiempo: 60,
+        descripcion:
+            "Un Pokémon de tipo agua que sufre de dolores de cabeza, lo que le permite usar poderes psíquicos.",
+        colores: ["amarillo", "blanco", "negro"],
+        oferta: "si",
+        precio: 160,
+    },
 ];
 
 // Ruta para obtener productos sin paginación
